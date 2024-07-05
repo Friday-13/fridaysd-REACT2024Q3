4024f8582e1b4036b5da9cd43677f55f0f2d2d38
Dmitrii Samsonenko <samsondm97it@gmail.com>
2024-07-06 02:04:44 +0300

    feat: add eslint and prettier configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,34 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-import pluginReactCompiler from "eslint-plugin-react-compiler";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import pluginReactCompiler from 'eslint-plugin-react-compiler';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-  plugins: {
-    'react-compiler':  pluginReactCompiler
-  },
-  "rules": {
-        "react-compiler/react-compiler": "error"
-    }
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    plugins: {
+      'react-compiler': pluginReactCompiler,
+    },
+    ignores: ['dist', '.eslint.config.js'],
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
   },
   { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReactConfig,
+  eslintPluginPrettierRecommended,
+  {
+    rules: {
+      'react-compiler/react-compiler': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+    },
+  },
 ];
