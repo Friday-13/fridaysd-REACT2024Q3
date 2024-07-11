f54ddd273d93dc1ea80cf5975162b1280fa61a7b
Dmitrii Samsonenko <samsondm97it@gmail.com>
2024-07-12 02:39:51 +0300

    feat: rewrite App component to functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { Component } from 'react';
 import Search from './views/search/search';
 import ErrorBoundary from './components/error-boundarie/error-boundarie';
-// import './App.module.scss';
 
-class App extends Component {
-  render() {
-    return (
-      <ErrorBoundary>
-        <Search />
-      </ErrorBoundary>
-    );
-  }
+function App() {
+  return (
+    <ErrorBoundary>
+      <Search />
+    </ErrorBoundary>
+  );
 }
 
 export default App;
