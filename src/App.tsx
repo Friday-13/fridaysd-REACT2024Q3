654e60547a15b99be9c1191db15f2a2b81ff13e8
Dmitrii Samsonenko <samsondm97it@gmail.com>
2024-07-13 00:53:00 +0300

    feat: add 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import Search from './views/search/search';
 import ErrorBoundary from './components/error-boundarie/error-boundarie';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Error from './views/error/error';
 
 function App() {
   return (
     <ErrorBoundary>
-      <Search />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Search />} />
+          <Route path="*" element={<Error code="404" message="Page not found" />} />
+        </Routes>
+      </BrowserRouter>
     </ErrorBoundary>
   );
 }
