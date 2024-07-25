d4bf43ba8b58f69086829c0a2c6923d2e66b283e
Dmitrii Samsonenko <samsondm97it@gmail.com>
2024-07-25 22:53:41 +0300

    feat: add reducer for people search

--- src/services/swapi.ts
+++ src/services/swapi.ts
@@ -1,6 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { IPerson } from '@services/api';
 
+export interface IPeopleSearchParams {
+  name?: string;
+  page?: number;
+}
+
 const BASE_URL = 'https://swapi.dev/api/';
 
 export const swApi = createApi({
@@ -10,10 +15,25 @@ export const swApi = createApi({
     getPersonById: builder.query<IPerson, string>({
       query: (personId) => `people/${personId}`,
     }),
+    getPeople: builder.query<Array<IPerson>, IPeopleSearchParams>({
+      query: ({ name, page }) => {
+        const params = new URLSearchParams();
+        if (name) {
+          params.append('search', name);
+        }
+
+        if (page !== undefined) {
+          params.append('page', `${page}`);
+        }
+        return `people?${params.toString()}`;
+      },
+    }),
   }),
 });
 
-export type { IPerson };
-
 export const useGetPersonByIdQuery: (personId: string) => ReturnType<typeof swApi.endpoints.getPersonById.useQuery> =
   swApi.endpoints.getPersonById.useQuery;
+
+export const useGetPeopleQuery: (
+  searchParams: IPeopleSearchParams
+) => ReturnType<typeof swApi.endpoints.getPeople.useQuery> = swApi.endpoints.getPeople.useQuery;
