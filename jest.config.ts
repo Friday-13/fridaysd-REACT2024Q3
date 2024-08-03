b9078643cf689dc8b4dd1b2cb7be6cd1a448591e
Dmitrii Samsonenko <samsondm97it@gmail.com>
2024-08-04 02:42:27 +0300

    test: update test config for next js

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,12 @@
-export default {
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   transform: {
@@ -19,3 +27,5 @@ export default {
     '^@components/(.*)$': '<rootDir>/components/$1',
   },
 };
+
+export default createJestConfig(config);
